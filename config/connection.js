48ad01503994114fc1de76989748c844f9848d6e
Akashanil21 <113817881+Akashanil21@users.noreply.github.com>
2022-12-15 12:07:21 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ const state = {
 }
 
 module.exports.connect = (done)=>{
-   // const url1 = 'mongodb+srv://akashanil:<password>@cluster0.6qxu3he.mongodb.net/?retryWrites=true&w=majority'
-    const url = 'mongodb://localhost:27017'
+    const url1 = 'mongodb+srv://akashanil:akashanil@cluster0.6qxu3he.mongodb.net'
+    //const url = 'mongodb://localhost:27017'
     const dbname = 'shopping'
 
-    mongoClient.connect(url,(err,data)=>{
+    mongoClient.connect(url1,(err,data)=>{
         if(err) return done(err)
         state.db = data.db(dbname)
         done()
